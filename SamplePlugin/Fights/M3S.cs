c1326aa8c090b215791b27487d2a42c4533eedd3
devckuw <team.devckuw@gmail.com>
2024-08-13 11:54:35 +0200

    dummy text for all fights

--- SamplePlugin/Fights/M3S.cs
+++ SamplePlugin/Fights/M3S.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImGuiNET;
 
 namespace combatHelper.Fights
 {
     public class M3S : Fight
     {
-        public override void Draw() { }
+        public override void Draw()
+        {
+            ImGui.Text("M3S figth.");
+        }
     }
 }
--- SamplePlugin/Fights/M4S.cs
+++ SamplePlugin/Fights/M4S.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImGuiNET;
 
 namespace combatHelper.Fights
 {
     public class M4S : Fight
     {
-        public override void Draw() { }
+        public override void Draw()
+        {
+            ImGui.Text("M4S figth.");
+        }
     }
 }
